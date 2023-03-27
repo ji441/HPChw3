0299c474c1fc743433edb69e46dcb5664fa321bc
qianhao_ji <qj2030@nyu.edu>
2023-03-26 21:00:56 -0400

    final version

--- gs2D-omp.cpp
+++ gs2D-omp.cpp
@@ -9,6 +9,33 @@ using namespace std;
 // g++ -std=c++11 -O3 -fopenmp -march=native gs2D-omp.cpp  && ./a.out
 // g++ -std=c++11 -O0 -fopenmp -march=native gs2D-omp.cpp -g
 // valgrind --leak-check=full ./a.out
+// g++ -std=c++11 -O3 -march=native gs2D-omp.cpp  && ./a.out
+double fnorm(int nthreads, double h, long N, double *u)
+{
+    double sum = 0;
+#if defined(_OPENMP)
+    omp_set_num_threads(nthreads);
+#pragma omp parallel for reduction(+ \
+                                   : sum)
+    for (int r = 1; r < N + 1; r++)
+    {
+        for (int c = 1; c < N + 1; c++)
+        {
+            sum += pow(abs(1 + (u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] - 4 * u[r + c * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / (h * h)), 2.0);
+        }
+    }
+#else
+    for (int r = 1; r < N + 1; r++)
+    {
+        for (int c = 1; c < N + 1; c++)
+        {
+            sum += pow(abs(1 + (u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] - 4 * u[r + c * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / (h * h)), 2.0);
+        }
+    }
+#endif
+
+    return sqrt(sum);
+}
 void parallel(int &nthreads, long &N)
 {
     double h = 1.0 / (N + 1);
@@ -20,6 +47,7 @@ void parallel(int &nthreads, long &N)
     }
     Timer tt;
     tt.tic();
+    double error; // original error is N
 #if defined(_OPENMP)
     omp_set_num_threads(nthreads);
     for (int i = 0; i < 1000; i++) // 1000 iterations
@@ -39,7 +67,7 @@ void parallel(int &nthreads, long &N)
             }
             for (c; c < N + 1; c += 2)
             {
-                u[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+                u[r + c * (N + 2)] = (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / 4.0;
             }
         }
 #pragma omp parallel for // update black then
@@ -56,12 +84,20 @@ void parallel(int &nthreads, long &N)
             }
             for (c; c < N + 1; c += 2)
             {
-                u[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+                u[r + c * (N + 2)] = (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / 4.0;
             }
+        }
+        // check for convergence
+
+        error = fnorm(nthreads, h, N, u);
+        if (error < N / 10000.0)
+        {
+            break;
         }
     }
 
-    printf("with %d threads,N = %ld,the time for 1000 iterations are: %.6f\n", nthreads, N, tt.toc());
+    printf("with %d threads,N = %ld,the time for 1000 iterations are: %.6f, error is: %f\n", nthreads, N, tt.toc(), error);
+
 #else
     for (int i = 0; i < 1000; i++)
     {
@@ -80,7 +116,7 @@ void parallel(int &nthreads, long &N)
             }
             for (c; c < N + 1; c += 2)
             {
-                u[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+                u[r + c * (N + 2)] = (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / 4.0;
             }
         }
         // update black then
@@ -97,18 +133,25 @@ void parallel(int &nthreads, long &N)
             }
             for (c; c < N + 1; c += 2)
             {
-                u[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+                u[r + c * (N + 2)] = 1.0 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
             }
         }
+        // check for convergence
+        error = fnorm(nthreads, h, N, u);
+        if (error < N / 10000.0)
+        {
+            break;
+        }
     }
-    printf("with one threads,N = %ld,the time for 1000 iterations are: %.6f\n", N, tt.toc());
+
+    printf("with one threads,N = %ld,the time for 1000 iterations are: %.6f, error is: %f\n", N, tt.toc(), error);
 #endif
     delete[] u;
 }
 int main()
 {
     int tarr[] = {1, 8, 16, 32};
-    long narr[] = {100, 1000, 2000};
+    long narr[] = {10, 100, 1000};
     for (int numthreads : tarr)
     {
         for (long N : narr)
--- jacobi2D-omp.cpp
+++ jacobi2D-omp.cpp
@@ -6,9 +6,35 @@
 #include <omp.h>
 #endif
 using namespace std;
-// g++ -std=c++11 -O2 -fopenmp -march=native jacobi2D-omp.cpp && ./a.out
+// g++ -std=c++11 -O3 -fopenmp -march=native jacobi2D-omp.cpp && ./a.out
 // g++ -std=c++11 -O0 -fopenmp -march=native jacobi2D-omp.cpp -g
 // valgrind --leak-check=full ./a.out
+double fnorm(int nthreads, double h, long N, double *u)
+{
+    double sum = 0;
+#if defined(_OPENMP)
+    omp_set_num_threads(nthreads);
+#pragma omp parallel for reduction(+ \
+                                   : sum)
+    for (int r = 1; r < N + 1; r++)
+    {
+        for (int c = 1; c < N + 1; c++)
+        {
+            sum += pow(abs(1 + (u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] - 4 * u[r + c * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / (h * h)), 2.0);
+        }
+    }
+#else
+    for (int r = 1; r < N + 1; r++)
+    {
+        for (int c = 1; c < N + 1; c++)
+        {
+            sum += pow(abs(1 + (u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] - 4 * u[r + c * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]) / (h * h)), 2.0);
+        }
+    }
+#endif
+
+    return sqrt(sum);
+}
 void parallel(int numthreads, long N)
 {
     double *u = new double[(N + 2) * (N + 2)];
@@ -18,6 +44,7 @@ void parallel(int numthreads, long N)
     {
         u[i] = 0;
     }
+    double error;
     // timer begins
     Timer tt;
     tt.tic();
@@ -27,16 +54,44 @@ void parallel(int numthreads, long N)
     omp_set_num_threads(numthreads); // set threads used
     for (int i = 0; i < 1000; i++)
     {
-        double *u_new = new double[(N + 2) * (N + 2)];
+        double *u_new = new double[(N + 2) * (N + 2)]; // container for new u values
 #pragma omp parallel for
         for (int r = 1; r < N + 1; r++)
         {
             for (int c = 1; c < N + 1; c++)
             {
-                u_new[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+                u_new[r + c * (N + 2)] = 1.0 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
             }
         }
 #pragma omp parallel for
+        for (int r = 1; r < N + 1; r++) // write the new vlaue into u
+        {
+            for (int c = 1; c < N + 1; c++)
+            {
+                u[r + c * (N + 2)] = u_new[r + c * (N + 2)];
+            }
+        }
+        delete[] u_new;
+        // check for convergence
+
+        error = fnorm(numthreads, h, N, u);
+        if (error < N / 10000.0)
+        {
+            break;
+        }
+    }
+    printf("with %d threads,N = %ld,the time for 1000 iterations are: %.6f, error is: %f\n", numthreads, N, tt.toc(), error);
+#else
+    for (int i = 0; i < 1000; i++)
+    {
+        double *u_new = new double[(N + 2) * (N + 2)];
+        for (int r = 1; r < N + 1; r++)
+        {
+            for (int c = 1; c < N + 1; c++)
+            {
+                u_new[r + c * (N + 2)] = 1.0 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
+            }
+        }
         for (int r = 1; r < N + 1; r++)
         {
             for (int c = 1; c < N + 1; c++)
@@ -45,35 +100,22 @@ void parallel(int numthreads, long N)
             }
         }
         delete[] u_new;
-    }
-    printf("with %d threads,N = %ld,the time for 1000 iterations are: %.6f\n", numthreads, N, tt.toc());
-#else
-    for (int i = 0; i < 1000; i++)
-    {
-        double u_new[(N + 2) * (N + 2)];
-        for (int r = 1; r < N + 1; r++)
+        // check for convergence
+
+        error = fnorm(numthreads, h, N, u);
+        if (error < N / 10000.0)
         {
-            for (int c = 1; c < N + 1; c++)
-            {
-                u_new[r + c * (N + 2)] = 1 / 4 * (h * h + u[r - 1 + c * (N + 2)] + u[r + (c - 1) * (N + 2)] + u[r + 1 + c * (N + 2)] + u[r + (c + 1) * (N + 2)]);
-            }
-        }
-        for (int r = 1; r < N + 1; r++)
-        {
-            for (int c = 1; c < N + 1; c++)
-            {
-                u[r + c * (N + 2)] = u_new[r + c * (N + 2)];
-            }
+            break;
         }
     }
-    printf("with one thread,N = %ld,the time for 1000 iterations are: %.6f\n", N, tt.toc());
+    printf("with one thread,N = %ld,the time for 1000 iterations are: %.6f\n,error is: %f\n", N, tt.toc(), error);
 #endif
     delete[] u;
 }
 int main()
 {
-    int tarr[] = {1, 8, 16, 32, 64};
-    long narr[] = {100, 1000, 2000};
+    int tarr[] = {1, 8, 16, 32};
+    long narr[] = {10, 100, 1000};
     for (int numthreads : tarr)
     {
         for (long N : narr)
